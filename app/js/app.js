76beaf9bb4aeb390de94ea2b2cb952637393d85c
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2015-11-08 11:49:22 -0200

    Separado e melhorado código

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,21 @@
 var theForm = document.getElementById( 'theForm' );
 
-function parseExpression() {
+function calculate(numerador, denominador, x_val) {
+  var num_derivada = nerdamer('diff('+numerador+')');
+  var den_derivada = nerdamer('diff('+denominador+')');
 
+  var num_derivada_resolvido = num_derivada.evaluate({x: x_val}).text();
+  var den_derivada_resolvido = den_derivada.evaluate({x: x_val}).text();
+
+  if(num_derivada_resolvido === 0 && den_derivada_resolvido === 0) {
+    return calculate(num_derivada.text(), den_derivada.text(), x_val);
+  } else {
+    return {
+      numerador: num_derivada_resolvido,
+      denominador: den_derivada_resolvido,
+      resultado: nerdamer(num_derivada_resolvido + '/' + den_derivada_resolvido).evaluate().text()
+    };
+  }
 }
 
 
@@ -18,26 +32,20 @@ new stepsForm( theForm, {
     var valor_x = data[2].value;
 
     // 20-(x^2)+2x^3
-    var nominador = nerdamer(data[0].value).evaluate({x: valor_x});
+    var numerador = nerdamer(data[0].value).evaluate({x: valor_x});
 
     // -x^4+2x+20
     var denominador = nerdamer(data[1].value).evaluate({x: valor_x});
 
-
-    // Verifica se ambos nominador e denominador sao iguals a 0
-    if(parseInt(nominador.text()) === 0 && parseInt(denominador.text()) === 0){
-
-      var nom_derivada = nerdamer('diff('+data[0].value+')');
-      var den_derivada = nerdamer('diff('+data[1].value+')');
-
-      var nom_derivada_resolvido = nom_derivada.evaluate({x: valor_x}).text();
-      var den_derivada_resolvido = den_derivada.evaluate({x: valor_x}).text();
+    // Verifica se ambos numerador e denominador sao iguals a 0
+    if(parseInt(numerador.text()) === 0 && parseInt(denominador.text()) === 0){
+      var resultado = calculate(data[0].value, data[1].value, data[2].value);
 
       var r = '';
       r += 'Problema RESOLVIDO!<br/>';
-      r += 'Deriavada do denominador: ' + nom_derivada_resolvido + '<br/>';
-      r += 'Deriavada do nominador: ' + den_derivada_resolvido + '<br/>';
-      r += 'Resultado final: ' + nerdamer(nom_derivada_resolvido + '/' + den_derivada_resolvido).evaluate().text();
+      r += 'Deriavada do numerador: ' + resultado.numerador + '<br/>';
+      r += 'Deriavada do denominador: ' + resultado.denominador + '<br/>';
+      r += 'Resultado final: ' + resultado.resultado;
 
       messageEl.innerHTML = r;
     } else {
